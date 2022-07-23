319b2a0da698a85b641d6bda0cf51c7a40e7a9f7
Ziyi Xu <ziyixu@berkeley.edu>
2022-07-23 11:13:14 +0800

    Upload all project code

---
+++ Project/Scheme/scheme/questions.scm
@@ -0,0 +1,92 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 15
+  (define (enumerate-help lst number)
+        (cond ((null? lst) ())
+              (else (cons (cons number (cons (car lst) nil)) (enumerate-help (cdr lst) (+ number 1))))))
+  (enumerate-help s 0)
+  )
+  ; END PROBLEM 15
+
+(define (reverse lst)
+  (define (reverse-tail sofar rest)
+    (if (null? rest)
+          sofar
+          (reverse-tail (cons (car rest) sofar) (cdr rest))))
+  (reverse-tail nil lst)
+)
+;; Problem 16
+;; Merge two lists LIST1 and LIST2 according to INORDER? and return
+;; the merged lists.
+(define (merge inorder? list1 list2)
+  ; BEGIN PROBLEM 16
+  (define (merge-helper inorder? list1 list2 current) 
+      (cond ((null? list1) (append (reverse list2) current))
+            ((null? list2) (append (reverse list1) current))
+            ((inorder? (car list1) (car list2)) (merge-helper inorder? (cdr list1) list2 (cons (car list1) current)))
+            (else (merge-helper inorder? list1 (cdr list2) (cons (car list2) current)))))
+    (reverse (merge-helper inorder? list1 list2 ()))    
+  )
+  ; END PROBLEM 16
+
+
+;; Optional Problem 1
+
+;; Returns a function that checks if an expression is the special form FORM
+(define (check-special form)
+  (lambda (expr) (equal? form (car expr))))
+
+(define lambda? (check-special 'lambda))
+(define define? (check-special 'define))
+(define quoted? (check-special 'quote))
+(define let?    (check-special 'let))
+
+;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define (let-to-lambda expr)
+  (cond ((atom? expr)
+         ; BEGIN PROBLEM 17
+         'replace-this-line
+         ; END PROBLEM 17
+         )
+        ((quoted? expr)
+         ; BEGIN PROBLEM 17
+         'replace-this-line
+         ; END PROBLEM 17
+         )
+        ((or (lambda? expr)
+             (define? expr))
+         (let ((form   (car expr))
+               (params (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 17
+           'replace-this-line
+           ; END PROBLEM 17
+           ))
+        ((let? expr)
+         (let ((values (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 17
+           'replace-this-line
+           ; END PROBLEM 17
+           ))
+        (else
+         ; BEGIN PROBLEM 17
+         'replace-this-line
+         ; END PROBLEM 17
+         )))
+
+
+
+;; Problem 21 (optional)
+;; Draw the hax image using turtle graphics.
+(define (hax d k)
+  ; BEGIN Question 21
+  'replace-this-line
+  )
+  ; END Question 21
